82ac9c1fb639d37af7d3c979cd10a03afe1325af
ZachWang <815810395@qq.com>
2020-02-21 20:12:09 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const bodyParser = require('body-parser');
+const hostname = 'localhost';
+const port = 3000;
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/', (req, res, next) => {
+    res.statusCode = 200;
+    res.end('POST operation');
+    //console.log(req.params.jstr);
+    console.log(req.body);
+});
+
+app.use(express.static(__dirname + '/public'));
+
+const server = http.createServer(app);
+server.listen(port, hostname, ()=> {
+    console.log(`Server running at http://${hostname}:${port}`)
+});
